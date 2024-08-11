1c5e40a9aa55abafbef520fb3b412f2e5aa98ac8
Vinicius P <viniciuspoa2@hotmail.com>
2024-08-11 13:41:56 -0300

    feat: altere subnet public name

--- public.tf
+++ public.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "eks_subnet_public_1a" {
+resource "aws_subnet" "eks_subnet_pub_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
   availability_zone       = "${data.aws_region.current.name}a"
@@ -8,14 +8,14 @@ resource "aws_subnet" "eks_subnet_public_1a" {
   tags = merge(
     local.tags,
     {
-      Name                     = "devops-subnet-1a",
+      Name                     = "${var.project_name}-subnet-1a",
       "kubernetes.io/role/elb" = 1
     }
   )
 }
 
 
-resource "aws_subnet" "eks_subnet_public_1b" {
+resource "aws_subnet" "eks_subnet_pub_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
   availability_zone       = "${data.aws_region.current.name}b"
@@ -24,7 +24,7 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   tags = merge(
     local.tags,
     {
-      Name                     = "devops-subnet-1b",
+      Name                     = "${var.project_name}-subnet-1b",
       "kubernetes.io/role/elb" = 1
     }
   )
