4d3a81f0b28c39d46af75e27c95061f14e946916
Vinicius P <viniciuspoa2@hotmail.com>
2024-08-26 15:40:04 -0300

    modules/eks_cluster/output.tf

--- modules/eks_cluster/output.tf
+++ modules/eks_cluster/output.tf
@@ -1,7 +1,17 @@
-# output "endpoint" {
-#   value = aws_eks_cluster
+output "cluster_name" {
+  value = aws_eks_cluster.aws_eks_cluster.name
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.aws_eks_cluster.endpoint
+}
+
+# output "certificate_authority" {
+#   value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 # }
 
-# output "kubeconfig-certificate-authority-data" {
-#   value = aws_eks_cluster.example.certificate_authority[0].data
-# }
+
+# Sainda do grupo de segurança do cluster
+output "cluster_sg" {
+  value = aws_eks_cluster.aws_eks_cluster.vpc_config[0].cluster_security_group_id
+}
