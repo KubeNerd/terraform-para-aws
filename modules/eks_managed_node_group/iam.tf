c2ca6ee3f5058e06d7289ba97324e955cfbfb1ef
Vinicius P <viniciuspoa2@hotmail.com>
2024-08-27 20:36:18 -0300

    feat: create iam role, and add variables to module

---
+++ modules/eks_managed_node_group/iam.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "eks_mng_role" {
+  name = "${var.project_name}-mng-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    var.tags,
+    {
+        Name = "AssumeRole-terraform-mng"
+    }
+  )
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_mng_role_attachment" {
+  role       = aws_iam_role.eks_mng_role.name
+  policy_arn = "arn:aws:iam::aws:policy/arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_mng_role_attachment" {
+  role       = aws_iam_role.eks_mng_role.name
+  policy_arn = "arn:aws:iam::aws:policy/arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_mng_role_attachment" {
+  role       = aws_iam_role.eks_mng_role.name
+  policy_arn = "arn:aws:iam::aws:policy/arn:aws:iam::aws:policy/arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
