caff9f2814bdb5601392a9eeffb6a466c793816a
Vinicius P <viniciuspoa2@hotmail.com>
2024-08-28 15:30:11 -0300

    fix: edit directory's name, and add my personal policy

--- modules.tf
+++ modules.tf
@@ -1,37 +1,44 @@
-module "eks_network" {
-  source       = "./modules/eks_network"
+module "s3" {
+  source       = "./modules/s3"
+  tags         = local.tags
+  project_name = var.project_name
+}
+
+module "network" {
+  source       = "./modules/network"
   cidr_block   = var.cidr_block
   project_name = var.project_name
   tags         = local.tags
 }
 
 
-module "eks_cluster" {
-  source       = "./modules/eks_cluster"
+module "cluster" {
+  source       = "./modules/cluster"
   project_name = var.project_name
   tags         = local.tags
 
   # Dependência entre módulos
-  public_subnet_1a = module.eks_network.subnet_pub_1a
-  public_subnet_1b = module.eks_network.subnet_pub_1b
+  public_subnet_1a = module.network.subnet_pub_1a
+  public_subnet_1b = module.network.subnet_pub_1b
 }
 
 
 
-module "eks_managed_node_group" {
-  source       = "./modules/eks_managed_node_group"
+module "managed_node_group" {
+  source       = "./modules/managed_node_group"
   tags         = local.tags
   project_name = var.project_name
 
   # Novamente usando a dependência entre módulos
-  cluster_name      = module.eks_cluster.cluster_name
-  private_subnet_1a = module.eks_network.subnet_priv_1a
-  private_subnet_1b = module.eks_network.subnet_priv_1b
+  cluster_name      = module.cluster.cluster_name
+  private_subnet_1a = module.network.subnet_priv_1a
+  private_subnet_1b = module.network.subnet_priv_1b
 
 }
 
-module "eks_s3" {
-  source       = "./modules/eks_s3"
-  tags         = local.tags
+
+module "aws_load_balancer_controller" {
+  source = "./modules/aws_load_balancer_controller"
   project_name = var.project_name
+  tags  = local.tags
 }
